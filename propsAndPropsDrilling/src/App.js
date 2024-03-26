58e5e3ec8c828fda1235fa314670eb45a9ebd1ef
Hayzed-codes <raheemhorlah@gmail.com>
2024-03-26 11:21:52 +0100

    Update

--- propsAndPropsDrilling/src/App.js
+++ propsAndPropsDrilling/src/App.js
@@ -1,85 +1,68 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
-import AddItem from './AddItem';
-
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import AddItem from "./AddItem";
 
 function App() {
-  // const name = "Hayzed";
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'A bag of Garri' 
-    },
-    {
-      id: 2,
-      checked: false,
-      item: 'Rice' 
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'Semo' 
-    },
-    {
-      id: 4,
-      checked: false,
-      item: 'Ponmo' 
-    },
-  ]);
-  const [newItem, setNewItem] = useState('')
-
-const handleCheck = (value) => {
-  // console.log(`key: ${id}`);
-  const listItems = items.map((item) => 
-    item.id  === value ? { ...item, checked: !item.checked } :item
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("Shoppinglist"))
   );
-  setItems(listItems);
-  localStorage.setItem('Shoppinglist', JSON.stringify(listItems))
-}
 
-const handleDelete = (del) => {
-  const listItems = items.filter((item) => item.id !== del);
-  setItems(listItems);
-  // localStorage.setItem('Delete', JSON.stringify(listItems))
-};
+  const [newItem, setNewItem] = useState("");
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if(!newItem) return;
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("Shoppinglist", JSON.stringify(newItems));
+  };
 
-  setNewItem('')
-}
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
 
- 
+  const handleCheck = (value) => {
+    const listItems = items.map((item) =>
+      item.id === value ? { ...item, checked: !item.checked } : item
+    );
+    setAndSaveItems(listItems);
+  };
+
+  const handleDelete = (del) => {
+    const listItems = items.filter((item) => item.id !== del);
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+
   return (
     <div className="App">
-      <Header title = "Welcome to props" />
-      <AddItem 
-      newItem = {newItem}
-      setNewItem = {setNewItem}
-      handleSubmit = {handleSubmit} 
+      <Header title="Welcome to props" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
       />
 
-
-      <Content 
-      items = {items}
-      handleCheck = {handleCheck}
-      handleDelete = {handleDelete}
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
-      <Footer length={items.length}/>
-
+      <Footer length={items.length} />
     </div>
-    
-    
-
-
   );
 }
 
-{/* JSX stands for JavaScript XML. It is simply a syntax extension of JavaScript. It allows us to directly write HTML in React (within JavaScript code). */}
+{
+  /* JSX stands for JavaScript XML. It is simply a syntax extension of JavaScript. It allows us to directly write HTML in React (within JavaScript code). */
+}
 export default App;
